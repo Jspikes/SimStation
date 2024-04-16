415f31b9a0571bfe9bb34665c0d65d37a8b4270f
Daisy <daisy.sandoval@sjsu.edu>
2024-04-16 16:55:09 -0700

    Added the five command classes to SimStation: Resume, Start, Stats, Stop, Suspend

---
+++ src/SimStation/StartCommand.java
@@ -0,0 +1,15 @@
+package SimStation;
+
+import mvc.*;
+
+public class StartCommand extends Command {
+    public StartCommand(Model model){
+        super(model);
+    }
+
+    @Override
+    public void execute(Model model) {
+        Simulation sim = (Simulation) model;
+        sim.start();
+    }
+}
---
+++ src/SimStation/StopCommand.java
@@ -0,0 +1,14 @@
+package SimStation;
+
+import mvc.*;
+
+public class StopCommand extends Command {
+    public StopCommand(Model model){
+        super(model);
+    }
+    @Override
+    public void execute(Model model) {
+        Simulation sim = (Simulation) model;
+        sim.stop();
+    }
+}
