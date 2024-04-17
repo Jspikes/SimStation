7830f92d813dc4651855d63af1e8a2f559b1d352
Jspikes <42878739+Jspikes@users.noreply.github.com>
2024-04-17 12:30:41 -0700

    couple more tweaks to plagueview

--- src/SimStation/plague/PlagueView.java
+++ src/SimStation/plague/PlagueView.java
@@ -9,16 +9,16 @@ import java.awt.*;
 public class PlagueView extends View {
     public PlagueView(Model model) {
         super(model);
-
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         PlagueSim plague = (PlagueSim) model;
         for(Agent a: plague.agents){
-            g.fillRect(a.xc, a.yc, 10, 10);
+            Peasant p = (Peasant) a;
+            if(p.infected){g.setColor(Color.RED);}
+            else {g.setColor(Color.GREEN);}
+            g.fillRect(p.xc, p.yc, 10, 10);
         }
     }
-
-    public void update(){}
 }
