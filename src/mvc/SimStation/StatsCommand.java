9c5b45e85d13316d12e9cdde7245eb53af3b8dfd
Agrika <109896751+agrikatheprogrammer@users.noreply.github.com>
2024-04-17 12:27:33 -0700

    Implementation without customizations
    Randomwalk and file command(New,save,etc) work and show on screen.

---
+++ src/mvc/SimStation/StatsCommand.java
@@ -0,0 +1,16 @@
+package mvc.SimStation;
+
+import mvc.Command;
+import mvc.Model;
+
+public class StatsCommand extends Command {
+
+    public StatsCommand(Model model) {
+        super(model);
+    }
+
+    @Override
+    public void execute() throws Exception {
+
+    }
+}
